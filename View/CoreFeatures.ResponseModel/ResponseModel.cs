79dfa571c34c306e724200eb99b5b357f6474e7b
BoguNoz <b.nozdryn@gmail.com>
2024-07-10 14:21:20 +0200

    10.07.2024 -v4

---
+++ View/CoreFeatures.ResponseModel/ResponseModel.cs
@@ -0,0 +1,30 @@
+﻿
+namespace CoreFeatures.ResposeModel
+{
+    /// <summary>
+    /// ResponseModel class represent framework of objects return by task in classes of View.Data Connection,  
+    /// and carries info about operation result. It was created in order to to make easier handling any exceptions that may occur while program is running 
+    /// on this layer
+    /// </summary>
+
+    public class ResponseModel
+    {
+        /// <summary>
+        /// Determines whether the operation was successful.
+        /// </summary>
+        /// <returns>True if successful, false otherwise.</returns>
+        public bool Status { get; set; }
+
+        /// <summary>
+        /// Extended message about operation result
+        /// </summary>
+        public string Message { get; set; } = string.Empty;
+
+
+        /// <summary>
+        /// Carries result of operation 
+        /// </summary>
+        public object Result { get; set; }
+
+    }
+}
